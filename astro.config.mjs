2746c272c87773c42ecd5c86131891810b0b8959
Isaya Muunganisha Wachumba <mariamussaofficial@gmail.com>
2026-08-19 12:22:59 +0300

    Configure production domain for Isaya website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://YOUR-DOMAIN.xyz',
+  site: 'https://isayamuunganishawachumba.xyz',
   integrations: [sitemap()],
   output: 'static',
   compressHTML: true,
